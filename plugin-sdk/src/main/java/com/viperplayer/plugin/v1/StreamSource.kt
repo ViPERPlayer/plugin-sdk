53322e7b5c5e26c2cceaa0d50d18c8944e8e0d08
Iscle <albertiscle9@gmail.com>
2026-01-08 02:01:05 +0100

    Update database version to 8; add replayGainDb and peakAmplitude fields to Song and SongEntity; refactor StreamSource and ViperMediaSource for improved handling of audio normalization

--- plugin-sdk/src/main/java/com/viperplayer/plugin/v1/StreamSource.kt
+++ plugin-sdk/src/main/java/com/viperplayer/plugin/v1/StreamSource.kt
@@ -7,7 +7,7 @@ import kotlinx.parcelize.Parcelize
  * Unified stream source that can be a URL, DASH XML, or AudioStream.
  * Uses a type discriminator to determine which variant is valid.
  * 
- * @property replayGain Optional ReplayGain value in dB to apply as player volume.
+ * @property replayGainDb Optional ReplayGain value in dB to apply as player volume.
  *                      Some plugins may only know this when requesting the stream.
  *                      Will be converted to linear when applied to player.
  * @property peakAmplitude Optional peak amplitude (0.0-1.0+). Some plugins may only know this when requesting the stream.
@@ -18,7 +18,7 @@ data class StreamSource(
     val url: String? = null,
     val dashXml: String? = null,
     val audioStream: AudioStream? = null,
-    val replayGain: Float? = null, // ReplayGain in dB
+    val replayGainDb: Float? = null, // ReplayGain in dB
     val peakAmplitude: Float? = null // Peak amplitude (0.0-1.0+)
 ) : Parcelable {
     enum class Type {
@@ -31,8 +31,8 @@ data class StreamSource(
     }
     
     companion object {
-        fun url(url: String, replayGain: Float? = null, peakAmplitude: Float? = null) = StreamSource(Type.URL, url = url, replayGain = replayGain, peakAmplitude = peakAmplitude)
-        fun dash(dashXml: String, replayGain: Float? = null, peakAmplitude: Float? = null) = StreamSource(Type.DASH, dashXml = dashXml, replayGain = replayGain, peakAmplitude = peakAmplitude)
-        fun audioStream(audioStream: AudioStream, replayGain: Float? = null, peakAmplitude: Float? = null) = StreamSource(Type.AUDIO_STREAM, audioStream = audioStream, replayGain = replayGain, peakAmplitude = peakAmplitude)
+        fun url(url: String, replayGainDb: Float? = null, peakAmplitude: Float? = null) = StreamSource(Type.URL, url = url, replayGainDb = replayGainDb, peakAmplitude = peakAmplitude)
+        fun dash(dashXml: String, replayGainDb: Float? = null, peakAmplitude: Float? = null) = StreamSource(Type.DASH, dashXml = dashXml, replayGainDb = replayGainDb, peakAmplitude = peakAmplitude)
+        fun audioStream(audioStream: AudioStream, replayGainDb: Float? = null, peakAmplitude: Float? = null) = StreamSource(Type.AUDIO_STREAM, audioStream = audioStream, replayGainDb = replayGainDb, peakAmplitude = peakAmplitude)
     }
 }
